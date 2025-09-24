e2f6a1e822ab61cdf848ed345342298714bd4f57
artemdost <dartem925@gmail.com>
2025-09-25 01:48:41 +0300

    keyboard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from vkbottle.bot import Bot, Message
 from dotenv import load_dotenv
+from vkbottle import Keyboard, KeyboardButtonColor, Text
 import os
 load_dotenv()
 
@@ -10,5 +11,22 @@ bot = Bot(token=token)
 @bot.on.message(text="Привет")
 async def hi_handler(message: Message):
     await message.answer("Привет")
+
+@bot.on.message(text="Выложить")
+async def choose_district(m: Message):
+    kb = (
+        Keyboard(one_time=False)  # обычная, не inline
+        .add(Text("Сормово"), color=KeyboardButtonColor.PRIMARY)
+        .add(Text("Автозавод"), color=KeyboardButtonColor.PRIMARY)
+        .row()
+        .add(Text("Нагорный"), color=KeyboardButtonColor.PRIMARY)
+        .add(Text("Советский"), color=KeyboardButtonColor.PRIMARY)
+        .add(Text("Сормово"), color=KeyboardButtonColor.PRIMARY)
+        .add(Text("Автозавод"), color=KeyboardButtonColor.PRIMARY)
+        .row()
+        .add(Text("Нагорный"), color=KeyboardButtonColor.PRIMARY)
+        .add(Text("Советский"), color=KeyboardButtonColor.PRIMARY)
+    )
+    await m.answer("Выбери район Нижнего Новгорода:", keyboard=kb.get_json())
 
 bot.run_forever()
